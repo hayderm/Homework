4de78e01b400e53c5e51dfa57aad489a2ff748fa
hayderm <hayderm@users.noreply.github.com>
2019-08-15 12:11:10 -0700

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,5 +1,19 @@
-a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-print([number for number in a if number % 2 == 0 ] )      
+import numpy as np 
 
+'''
+L1 : first list length 
+L2 : second list length
 
+'''
+L1 = int(raw_input('Enter first list lenght:'))
+L2 = int(raw_input('Enter second list lenght:'))
+
+#generate first and second lists 
+list1 = np.random.randint(1,high=10,size=L1,dtype=int)
+list2 = np.random.randint(1,high=10,size=L2,dtype=int)
+print list1
+print list2
+for y in list1:
+    z = [y for x in list2 if y==x]
+print z
 
